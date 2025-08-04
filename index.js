a723d44246e5ca23565f138d6618c28e1f9177ab
anon1y4012 <github@silentmail.org>
2025-08-04 12:54:53 -0500

    Updating to 2.0

--- index.js
+++ index.js
@@ -1,75 +1,118 @@
 const axios = require('axios');
-const api = axios.create({});
-
-var Service, Characteristic;
-
-const DEF_MIN_RATE = -10000,
-      DEF_MAX_RATE = 10000;
-
-const interval = 5; // Minutes
 
 const PLUGIN_NAME = 'homebridge-energy-price';
 const ACCESSORY_NAME = 'Energy Price';
 
-module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, EnergyPrice);
-}
+/**
+ * This is the modern way of registering a plugin with Homebridge.
+ * It provides the `api` object, which is used to access HAP (HomeKit Accessory Protocol) services and characteristics.
+ */
+module.exports = (api) => {
+    api.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, EnergyPrice);
+};
+
+// --- CONSTANTS ---
+const DEF_MIN_RATE = -10000;
+const DEF_MAX_RATE = 10000;
+const DEFAULT_INTERVAL_MINUTES = 5;
 
 class EnergyPrice {
     constructor(log, config, api) {
         this.log = log;
         this.config = config;
         this.api = api;
-        this.name = config["name"];
-        this.manufacturer = config["manufacturer"] || "Energy Price";
-        this.model = config["model"] || "Monitor";
-        this.minRate = config["min_rate"] || DEF_MIN_RATE;
-        this.maxRate = config["max_rate"] || DEF_MAX_RATE;
-        this.refreshInterval = config["refreshInterval"] === undefined ? (interval * 60000) : (config["refreshInterval"] * 60000);
-        
-        this.service = new Service.TemperatureSensor(this.name);
-        this.service.getCharacteristic(Characteristic.CurrentTemperature)
-            .on('get', this.handleCurrentTemperatureGet.bind(this));
-        
-        this.informationService = new Service.AccessoryInformation()
-            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-            .setCharacteristic(Characteristic.Model, this.model);
 
-        // Event handling for Homebridge 2.0
-        if (api) {
-            api.on('didFinishLaunching', this.poll.bind(this));
-        } else {
-            setTimeout(this.poll.bind(this), 1000);
-        }
+        // Store HAP objects
+        this.Service = this.api.hap.Service;
+        this.Characteristic = this.api.hap.Characteristic;
+
+        // Accessory information
+        this.name = config.name || ACCESSORY_NAME;
+        this.manufacturer = config.manufacturer || "Energy Price"; //
+        this.model = config.model || "Monitor"; //
+
+        // Configuration
+        this.minRate = config.min_rate || DEF_MIN_RATE; //
+        this.maxRate = config.max_rate || DEF_MAX_RATE; //
+        const refreshIntervalMinutes = config.refreshInterval === undefined ? DEFAULT_INTERVAL_MINUTES : config.refreshInterval; //
+        this.refreshInterval = refreshIntervalMinutes * 60000;
+
+        // Create a new TemperatureSensor service
+        this.service = new this.Service.TemperatureSensor(this.name);
+        this.service.getCharacteristic(this.Characteristic.CurrentTemperature)
+            .on('get', this.handleCurrentTemperatureGet.bind(this)); //
+
+        // Create AccessoryInformation service
+        this.informationService = new this.Service.AccessoryInformation()
+            .setCharacteristic(this.Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(this.Characteristic.Model, this.model); //
+
+        // Create an axios instance for API calls
+        this.axios = axios.create({});
+
+        /**
+         * The 'didFinishLaunching' event is fired when Homebridge has restored all cached accessories.
+         * We use this to start our polling logic.
+         */
+        this.api.on('didFinishLaunching', () => {
+            this.log.debug('Did finish launching, starting poll.');
+            this.poll();
+        });
     }
 
+    /**
+     * This method polls the ComEd API for the latest price and updates the HomeKit characteristic.
+     */
     async poll() {
-        if (this.timer) clearTimeout(this.timer);
-        this.timer = null;
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+
         try {
-            const hourlyData = await api.get('https://hourlypricing.comed.com/api?type=currenthouraverage');
-            this.log.info('Data from API', hourlyData.data[0].price);
-            if (hourlyData.data[0].price == null) {
-                this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(DEF_MAX_RATE);
+            const response = await this.axios.get('https://hourlypricing.comed.com/api?type=currenthouraverage');
+            const price = parseFloat(response.data[0].price);
+            this.log.info(`Current energy price: ${price} cents/kWh`);
+
+            if (isNaN(price)) {
+                this.log.warn('Received invalid price data from API. Setting characteristic to max rate.');
+                this.service.getCharacteristic(this.Characteristic.CurrentTemperature).updateValue(this._priceToCelsius(this.maxRate));
             } else {
-                this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.convertToFahrenheit(hourlyData.data[0].price), 1);
+                const tempValue = this._priceToCelsius(price);
+                this.service.getCharacteristic(this.Characteristic.CurrentTemperature).updateValue(tempValue);
             }
         } catch (error) {
-            this.log.error('Error getting current billing rate %s', error);
-            this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(DEF_MAX_RATE);
+            this.log.error(`Error getting current billing rate: ${error.message}`);
+            this.service.getCharacteristic(this.Characteristic.CurrentTemperature).updateValue(this._priceToCelsius(this.maxRate));
         }
-        this.timer = setTimeout(this.poll.bind(this), this.refreshInterval);
+
+        this.timer = setTimeout(() => this.poll(), this.refreshInterval);
     }
 
-    convertToFahrenheit(value) {
-        return (value - 32) * 5 / 9;
+    /**
+     * This function converts the energy price (in cents) into a Celsius value.
+     * The math is a clever hack to make the Home app display the price in cents
+     * as a Fahrenheit temperature value.
+     *
+     * How it works:
+     * 1. Price in cents, P, is the input (e.g., 2.5).
+     * 2. This function calculates a Celsius temperature, Tc = (P - 32) * 5 / 9.
+     * 3. Homebridge reports Tc to HomeKit.
+     * 4. If the user's iOS device is set to Fahrenheit, the Home app converts it back:
+     * Tf = (Tc * 9 / 5) + 32
+     * Tf = (((P - 32) * 5 / 9) * 9 / 5) + 32  ->  (P - 32) + 32  ->  P
+     * 5. The result is the price in cents is displayed as degrees Fahrenheit (e.g., "2.5 °F").
+     *
+     * I have renamed the original 'convertToFahrenheit' function to '_priceToCelsius' 
+     * to more accurately reflect what it does.
+     */
+    _priceToCelsius(price) {
+        return (price - 32) * 5 / 9;
     }
 
     handleCurrentTemperatureGet(callback) {
-        this.log.info('Current temperature get requested');
-        callback(null, this.service.getCharacteristic(Characteristic.CurrentTemperature).value);
+        this.log.debug('Get current temperature requested');
+        callback(null, this.service.getCharacteristic(this.Characteristic.CurrentTemperature).value);
     }
 
     getServices() {
